d6b3ce4457269e8148c96ba56271ef6c22ef1f6e
Fyrope83 <0502741@schoolsnet.act.edu.au>
2026-09-07 11:06:04 +1000

    weapon management started

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -132,7 +132,6 @@ func _physics_process(delta): #Occurs every delta frame
 	if not is_multiplayer_authority(): return
 	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	#MOVEMENT AND CONTROLS
 	move_and_slide()
 	
---
+++ WeaponManager/Weapon_data.gd
@@ -0,0 +1,5 @@
+class_name WeaponData extends Resource
+
+#@export var weapon: Weapon
+@export var unlocked: bool = false
+@export var ammo: int = 0
