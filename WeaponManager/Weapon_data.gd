ed0eb2b2007fdb08fcfd5e338682b7fc42f9cef5
Fyrope83 <0502741@schoolsnet.act.edu.au>
2026-09-16 11:59:28 +1000

    starting the weapons manager

--- WeaponManager/Weapon_data.gd
+++
@@ -1,5 +0,0 @@
-class_name WeaponData extends Resource
-
-#@export var weapon: Weapon
-@export var unlocked: bool = false
-@export var ammo: int = 0
---
+++ assets/Weapons/Scripts/pistol.gd
@@ -0,0 +1,7 @@
+class_name WeaponData extends Resource
+
+@export var weapon_name: String = "Pistol"
+@export var damage: float = 2.0
+@export var max_ammo: int = 12
+@export var weapon_model: PackedScene
+@export var weapon_position: Vector3 = Vector3(0.5, -0.186, -0.5)
